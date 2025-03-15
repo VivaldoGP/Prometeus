07704d4011955dfe9953ee80f08cc96bb799a2d6
VivaldoGP <VivaldoGP27@hotmail.com>
2025-03-14 23:13:21 -0600

    test: test searching products in CDSE

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,10 +2,11 @@ from prometeus.search import get_collections
 from prometeus.prometeus import CopernicusAPI
 import requests
 
+
 def test_search_collections():
     api = CopernicusAPI()
-    products = api.search(mission_id='Sentinel2', max_records=20, end_date="2025-03-13", cloud_cover=20)
-    print(products)
+    products = api.search(mission_id='Sentinel2', max_records=20, cloud_cover=20, start_date="2025-03-09", end_date="2025-03-14")
+    print(products.json())
     assert products is not None
 
 def test_os_url(start_date: str = "2025-03-09", end_date: str = "2025-03-14"):
